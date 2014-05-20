23eb5ce1940cc5c299c7d54bf6359cad10255799
Cian Booth <this.is.cian@gmail.com>
2014-05-20 15:33:02 +0000

    glut is now working

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-
+[[ $1 == "clean" ]] && rm -r build 2</dev/null
 mkdir build 2>/dev/null || exit 1; 
 cd build || exit 2;
 cmake .. || exit 3;
--- src/game.cpp
+++ src/game.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 class Settings {
 	public:
-		int exit;
+		int exit, width, height;
 		bool floor;
 		Settings() {	
 			exit=0;
+			width=800;
+			height=600;
 			floor=false;
 		}
 };
@@ -17,6 +19,8 @@ Settings get_settings(int ac, char* av[]) {
 	po::options_description desc("Allowed options");
 	desc.add_options()
 		("help,h", "produce help message")
+		("width,w", "set width of display");
+		("height,h", "set height of display");
 		("floor,f", "floor it");
 	po::variables_map vm;
 	po::store(po::parse_command_line(ac,av,desc),vm);
@@ -30,17 +34,46 @@ Settings get_settings(int ac, char* av[]) {
 	
 	if(vm.count("floor")) {
 		set.floor=true;	
-		return set;
+	}
+	if(vm.count("width")) {
+		set.width=vm["width"].as<int>();	
+	}
+	if(vm.count("height")) {
+		set.height=vm["width"].as<int>();	
 	}
 	return set;
 }
 
+int initialiseWindow(int* argc, char* argv[], int w, int h) {
+	glutInit(argc,argv);
+	glutInitWindowPosition(-1,-1); // allow wm to decide where window opens
+	glutInitWindowSize(w,h);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
+	glutCreateWindow(Game_TITLE);
+	return 0;
+}
+
+void renderScene() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glBegin(GL_TRIANGLES);
+		glVertex3f(-0.5,0.0,0.0);
+		glVertex3f(0.0,1.0,0.0);
+		glVertex3f(0.5,0.0,0.0);
+	glEnd();
+	glutSwapBuffers();
+}
 
 int main(int argc, char* argv[]) {
-	Settings set=get_settings(argc,argv);
-	if(set.exit != 0) { return set.exit; }
+	Settings settings=get_settings(argc,argv);
+	if(settings.exit != 0) { return settings.exit; }
+
+	initialiseWindow(&argc, argv, settings.width, settings.height);
+	glutDisplayFunc(renderScene);
+	glutMainLoop();
+
 	double input=atof(argv[1]), output=sqrt(input);
-	if(set.floor) { output=floor(output); }
+
+	if(settings.floor) { output=floor(output); }
 	cout << "sqrt: " << output << endl;
 	return 0;
 }
